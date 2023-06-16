00848c2ca6da642c37cc1d6c1ed7adf88552eb14
George Stefan <george.stefan@nxp.com>
2023-06-16 13:15:28 +0300

    k32w1: add ot_cli_ftd_zc
    ot_cli_ftd_zc example for multiprotocol support (dual PAN)
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>

---
+++ examples/hybrid/ot_cli_ftd_zc/k32w1/app_preinclude.h
@@ -0,0 +1,41 @@
+/*! *********************************************************************************
+ *
+ * Copyright 2023 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ ********************************************************************************** */
+
+#ifndef _APP_PREINCLUDE_H_
+#define _APP_PREINCLUDE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Avoid the inclusion of empty files found in ZB example source dir (ZB repo) */
+#include "pdum_gen.h"
+#include "zps_gen.h"
+
+#define DUAL_MODE_APP
+
+#define TRACE_APP TRUE
+#define TRACE_ZCL TRUE
+
+extern void otCliOutputFormat(const char *aFmt, ...);
+
+#undef DBG_vPrintf
+#define DBG_vPrintf(STREAM, FORMAT, ARGS...)     \
+    do                                           \
+    {                                            \
+        if (STREAM)                              \
+        {                                        \
+            otCliOutputFormat((FORMAT), ##ARGS); \
+        }                                        \
+    } while (0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _APP_PREINCLUDE_H_ */
