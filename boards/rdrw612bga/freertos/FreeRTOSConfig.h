f8bc88d2900913463f4676b327ea1bb71beae339
Cristian Bulacu <cristian.bulacu@nxp.com>
2023-10-27 16:39:24 +0300

    DUT becomes unresponsive during RX/TX
    Increased lwip tcpip thread stack size by 1024 bytes.
    Also increased configTOTAL_HEAP_SIZE.
    
    Updates:
    	boards/rdrw612bga/freertos/FreeRTOSConfig.h
    	src/common/lwip/lwipopts.h
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

--- boards/rdrw612bga/freertos/FreeRTOSConfig.h
+++ boards/rdrw612bga/freertos/FreeRTOSConfig.h
@@ -76,7 +76,7 @@ LOG_MODULE_DEFINE(LOG_MODULE_NAME, kLOG_LevelDebug);
 #define configMAX_PRIORITIES (10)
 #define configMINIMAL_STACK_SIZE ((uint16_t)1024)
 #define configAPPLICATION_ALLOCATED_HEAP 1
-#define configTOTAL_HEAP_SIZE ((size_t)(158 * 1024))
+#define configTOTAL_HEAP_SIZE ((size_t)(159 * 1024))
 #define configSUPPORT_STATIC_ALLOCATION 0
 #define configSUPPORT_DYNAMIC_ALLOCATION 1
 #define configFRTOS_MEMORY_SCHEME 4
